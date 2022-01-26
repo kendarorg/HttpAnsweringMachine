40ba56b25e4256108affec991e77c9f3371dc471
Kendar <github@kendar.org>
2022-01-26 16:36:23 +0100

    STart generating pacts and calls

--- js.filters/src/main/java/org/kendar/servers/http/generators/CurlGenerator.java
+++ js.filters/src/main/java/org/kendar/servers/http/generators/CurlGenerator.java
@@ -5,6 +5,10 @@ import org.kendar.servers.http.Response;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.Base64;
+import java.util.Locale;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Component
 public class CurlGenerator implements BaseGenerator{
@@ -22,7 +26,32 @@ public class CurlGenerator implements BaseGenerator{
     public String generate(Request request, Response response) {
         var result = new ArrayList<String>();
         result.add("#!/bin/sh");
-        result.add("curl https://www.google.com");
+
+        var dataFile = UUID.randomUUID().toString()+".data";
+        if(!request.isBinaryRequest()){
+            var base64Bytes = Base64.getEncoder().encode(request.getRequestBytes());
+            result.add("echo '"+base64Bytes+"' |base64 -d -o "+dataFile);
+        }
+
+        var target = request.getProtocol()+"://"+request.getHost()+":"+request.getPort()+"/"+request.getPath();
+        target+="?"+String.join("&",
+                request.getQuery().entrySet().stream().map(e->e.getKey()+"="+e.getValue()).collect(Collectors.toList()));
+        result.add("curl -X "+request.getMethod().toUpperCase(Locale.ROOT)+"\\");
+        for (var head :
+                request.getHeaders().entrySet()){
+            result.add("\t-H '"+head.getKey()+":"+head.getValue()+"'\\");
+        }
+        if((request.getRequestBytes()!=null &&request.getRequestBytes().length>0 ) ||
+                (request.getRequestText()!=null && request.getRequestText().length()>0)){
+            result.add("\t-F 'file=@/"+dataFile+"'\\");
+        }
+        for (var postParameter :
+                request.getPostParameters().entrySet()){
+            result.add("\t-F \""+postParameter.getKey()+"="+postParameter.getValue()+"\"\\");
+        }
+        result.add("\t"+target);
+
+
         result.add("");
         return String.join("\n",result);
     }
