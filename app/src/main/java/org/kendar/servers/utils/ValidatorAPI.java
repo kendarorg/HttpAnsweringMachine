dbf62477736ae9e54d3e1e236ff320bd62494cbe
Kendar <github@kendar.org>
2022-01-27 15:48:53 +0100

    Searching for xsd generators

--- app/src/main/java/org/kendar/servers/utils/ValidatorAPI.java
+++ app/src/main/java/org/kendar/servers/utils/ValidatorAPI.java
@@ -18,8 +18,24 @@ import org.kendar.servers.http.Response;
 import org.kendar.servers.utils.models.ValidatorData;
 import org.kendar.servers.utils.models.ValidatorResult;
 import org.springframework.stereotype.Component;
+import org.wiztools.xsdgen.ParseException;
+import org.wiztools.xsdgen.XsdGen;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+import org.xml.sax.helpers.DefaultHandler;
 
+import javax.xml.XMLConstants;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.EnumSet;
 import java.util.Set;
 
@@ -76,4 +92,55 @@ public class ValidatorAPI implements FilteringClass {
         res.setStatusCode(code);
         res.setResponseText(mapper.writeValueAsString(result));
     }
+
+    @HttpMethodFilter(
+            phase = HttpFilterType.API,
+            pathAddress = "/api/utils/schemavalidator/xml",
+            method = "POST",
+            id = "1000a4b4-29tad-1jsc-9621-0ww2ac130002")
+    public void validateXml(Request req, Response res) throws IOException, ParseException, SAXException, ParserConfigurationException {
+        var data = mapper.readValue(req.getRequestText(), ValidatorData.class);
+        if(data.getSchema()==null || data.getSchema().length()==0){
+            XsdGen gen = new XsdGen();
+            try(var sourceStream = new ByteArrayInputStream(data.getTemplate().getBytes())) {
+                gen.parse(sourceStream);
+                try(ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+                    gen.write(out);
+                    data.setSchema(new String( out.toByteArray(), StandardCharsets.UTF_8 ));
+                }
+            }
+        }
+
+        var result = new ValidatorResult();
+
+        
+        SchemaFactory schemaFactory = SchemaFactory
+                .newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        try(var schemaStream = new ByteArrayInputStream(data.getSchema().getBytes())) {
+            Schema schema = schemaFactory.newSchema(new StreamSource(schemaStream));
+            SAXParserFactory saxFactory = SAXParserFactory.newInstance();
+            saxFactory.setSchema(schema);
+            SAXParser parser = saxFactory.newSAXParser();
+            try(var sourceStream = new ByteArrayInputStream(data.getSource().getBytes())) {
+                parser.parse(new InputSource(sourceStream), new DefaultHandler() {
+                    // TODO: other handler methods
+                    @Override
+                    public void error(SAXParseException e) throws SAXException {
+                        result.setError(true);
+                        result.getErrors().add(e.getMessage());
+                        throw e;
+                    }
+                });
+            }
+        }
+
+
+        res.setStatusCode(200);
+        res.addHeader("Content-type", "application/json");
+        if(result.getErrors().size()>0) {
+            res.setStatusCode(500);
+        }
+        res.setResponseText(mapper.writeValueAsString(result));
+    }
+
 }
