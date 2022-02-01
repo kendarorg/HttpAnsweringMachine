8cc22de615c2d73f325913d0254c82f3644b5675
Kendar <github@kendar.org>
2022-02-01 17:45:01 +0100

    Demoable

--- utils/docker/start/services/11-samplefe.sh
+++ utils/docker/start/services/11-samplefe.sh
@@ -2,18 +2,14 @@
 
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk/
 export PATH="${JAVA_HOME}/bin:${PATH}"
-export CLASSPATH=/start/services/answering/libs
 
-cd /start/services/answering
+cd /start/services/samplefe
 #java -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=0.0.0.0:5005 -Dother.dns=127.0.0.11 \
 #	"-Dloader.path=/start/services/answering/libs" -Dloader.main=org.kendar.Main -jar app-1.0-SNAPSHOT.jar \
 #	 org.springframework.boot.loader.PropertiesLauncher &
-	 
 
-java "-Dloader.path=/start/services/answering/libs"  -Dloader.main=org.kendar.Main  \
-	-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=0.0.0.0:5005 \
-	 -Dother.dns=127.0.0.11 -Djdk.tls.acknowledgeCloseNotify=true \
-	 -jar app-1.0-SNAPSHOT.jar org.springframework.boot.loader.PropertiesLauncher &
+
+java -jar fe-1.0-SNAPSHOT.jar &
 	 
 	 
 #java -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=0.0.0.0:5005 -Dother.dns=127.0.0.11 \
