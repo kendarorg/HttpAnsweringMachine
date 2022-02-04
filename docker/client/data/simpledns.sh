8f3c9c9810f835270db69213c7eab03d13356c49
Kendar <github@kendar.org>
2022-02-04 10:24:52 +0100

    Setup docker main images

---
+++ docker/client/data/simpledns.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk/
+export PATH="${JAVA_HOME}/bin:${PATH}"
+
+java -Dother.dns=127.0.0.11,${DNS_HIJACK_SERVER} -jar /etc/service/simpledns/simpledns-1.0-SNAPSHOT.jar
