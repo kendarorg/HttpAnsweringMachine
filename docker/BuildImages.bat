eb819c76c48a584bd6d3996eacfc48234bbdf845
Kendar <github@kendar.org>
2022-02-04 10:24:52 +0100

    Setup docker main images

---
+++ docker/BuildImages.bat
@@ -0,0 +1,22 @@
+﻿set ROOT_PWD=root
+set HAM_DEBUG=false
+set DNS_HIJACK_SERVER=THEDOCKERNAMEOFTHERUNNINGMASTER
+
+mkdir master\data\
+
+
+cd base
+docker build -t ham.base .
+
+cd ../master
+md "data\app" 2>NUL
+md "data\app\libs" 2>NUL
+copy /Y ..\..\app\target\*.jar data\app\
+copy /Y ..\..\libs\*.jar data\app\libs\
+docker build -t ham.master .
+
+cd ../client
+docker build -t ham.client .
+
+cd ../openvpn
+docker build -t ham.openvpn .
