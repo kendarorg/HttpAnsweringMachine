a701e1ec3922816895e29da60f34dbe27acb1607
Kendar <github@kendar.org>
2022-02-04 16:37:55 +0100

    Awk now resolving correctly

--- docker/images/openvpn/data/openvpn.sh
+++ docker/images/openvpn/data/openvpn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-dnsServer=`ping  $DNS_HIJACK_SERVER| grep -Eo "([0-9]+\.?){4}"|head -n 1`
-sed -i 's/push "dhcp-option DNS master.local.self"/push "dhcp-option DNS $dnsServer"/g' /etc/openvpn/openvpn.conf
+dnsServer=`ping -c 4 $DNS_HIJACK_SERVER|head -n 1| grep -Eo "([0-9]+\.?){4}"`
+sed -i "s%push \"dhcp-option DNS master.local.self\"%push \"dhcp-option DNS $dnsServer\"%g" /etc/openvpn/openvpn.conf
 /usr/local/bin/ovpn_run
 
 sleep infinite
