94b2c1b52d01a44f3bd67f7c2b40c0ca42a1e15e
Kendar <github@kendar.org>
2022-02-18 17:24:10 +0100

    Adding custom schema validator against templates

--- ham/app/src/main/java/org/kendar/servers/utils/ValidatorAPI.java
+++ ham/app/src/main/java/org/kendar/servers/utils/ValidatorAPI.java
@@ -3,13 +3,13 @@ package org.kendar.servers.utils;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.xml.XmlMapper;
-import com.fasterxml.jackson.module.jsonSchema.JsonSchema;
 import com.networknt.schema.JsonSchemaFactory;
-import com.networknt.schema.SchemaValidatorsConfig;
 import com.networknt.schema.SpecVersionDetector;
 import com.networknt.schema.ValidationMessage;
-import com.saasquatch.jsonschemainferrer.*;
+import com.saasquatch.jsonschemainferrer.AdditionalPropertiesPolicies;
+import com.saasquatch.jsonschemainferrer.JsonSchemaInferrer;
+import com.saasquatch.jsonschemainferrer.RequiredPolicies;
+import com.saasquatch.jsonschemainferrer.SpecVersion;
 import org.kendar.http.FilteringClass;
 import org.kendar.http.HttpFilterType;
 import org.kendar.http.annotations.HttpMethodFilter;
@@ -18,6 +18,8 @@ import org.kendar.servers.http.Request;
 import org.kendar.servers.http.Response;
 import org.kendar.servers.utils.models.ValidatorData;
 import org.kendar.servers.utils.models.ValidatorResult;
+import org.kendar.xml.DiffInferrer;
+import org.kendar.xml.model.XmlException;
 import org.springframework.stereotype.Component;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
@@ -32,16 +34,15 @@ import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.EnumSet;
+import java.util.HashSet;
 import java.util.Set;
 
 @Component
 @HttpTypeFilter(hostAddress = "${global.localAddress}", blocking = true)
 public class ValidatorAPI implements FilteringClass {
     ObjectMapper mapper = new ObjectMapper();
+    DiffInferrer diffInferrer = new DiffInferrer();
     private static final JsonSchemaInferrer inferrer = JsonSchemaInferrer.newBuilder()
             .setSpecVersion(SpecVersion.DRAFT_04)
             // Requires commons-validator
@@ -70,25 +71,30 @@ public class ValidatorAPI implements FilteringClass {
             method = "POST",
             id = "1000a4b4-29tad-1jsc-9621-0242ac130002")
     public void validate(Request req, Response res) throws IOException {
+        Set<ValidationMessage> errors = new HashSet<>();
+        var result = new ValidatorResult();
+        result.setError(false);
+
         var data = mapper.readValue(req.getRequestText(), ValidatorData.class);
         if(data.getSchema()==null || data.getSchema().length()==0){
-            final var sample = mapper.readTree(data.getTemplate());
-            final var resultForSample = inferrer.inferForSample(sample);
-            data.setSchema(mapper.writeValueAsString(resultForSample));
+            try {
+                diffInferrer.diff(data.getTemplate(), data.getSource());
+            }catch (XmlException ex){
+                result.setError(true);
+                result.getErrors().add(ex.getMessage());
+            }
+        }else {
+            var schema = getJsonSchemaFromStringContent(data.getSchema());
+            var toVerify = getJsonNodeFromStringContent(data.getSource());
+            errors = schema.validate(toVerify);
+            for (var message : errors) {
+                result.setError(true);
+                result.getErrors().add(message.getMessage());
+            }
         }
-        var schema = getJsonSchemaFromStringContent(data.getSchema());
-        var toVerify = getJsonNodeFromStringContent(data.getSource());
-        Set<ValidationMessage> errors = schema.validate(toVerify);
 
-        var result = new ValidatorResult();
-        var code = 200;
-        for (var message : errors) {
-            result.setError(true);
-            code = 200;
-            result.getErrors().add(message.getMessage());
-        }
+
         res.addHeader("Content-type", "application/json");
-        res.setStatusCode(code);
         res.setResponseText(mapper.writeValueAsString(result));
     }
 
@@ -102,44 +108,34 @@ public class ValidatorAPI implements FilteringClass {
 
         var result = new ValidatorResult();
         if(data.getSchema()==null || data.getSchema().length()==0){
-            XmlMapper xmlMapper = new XmlMapper();
-            JsonNode templateNode = xmlMapper.readTree(data.getTemplate().getBytes());
-            data.setTemplate(mapper.writeValueAsString(templateNode));
+            try {
+                diffInferrer.diff(data.getTemplate(), data.getSource());
+            }catch (XmlException ex){
+                result.setError(true);
+                result.getErrors().add(ex.getMessage());
+            }
+        }else {
+            SchemaFactory schemaFactory = SchemaFactory
+                    .newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            try (var schemaStream = new ByteArrayInputStream(data.getSchema().getBytes())) {
+                Schema schema = schemaFactory.newSchema(new StreamSource(schemaStream));
+                SAXParserFactory saxFactory = SAXParserFactory.newInstance();
+                saxFactory.setSchema(schema);
+                SAXParser parser = saxFactory.newSAXParser();
+                try (var sourceStream = new ByteArrayInputStream(data.getSource().getBytes())) {
 
-            JsonNode sourceNode = xmlMapper.readTree(data.getSource().getBytes());
-            data.setSource(mapper.writeValueAsString(sourceNode));
-            req.setRequestText(mapper.writeValueAsString(data));
-            this.validate(req,res);
-            return;
-        }
-
-
-
-        SchemaFactory schemaFactory = SchemaFactory
-                .newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-        try(var schemaStream = new ByteArrayInputStream(data.getSchema().getBytes())) {
-            Schema schema = schemaFactory.newSchema(new StreamSource(schemaStream));
-            SAXParserFactory saxFactory = SAXParserFactory.newInstance();
-            saxFactory.setSchema(schema);
-            SAXParser parser = saxFactory.newSAXParser();
-            try(var sourceStream = new ByteArrayInputStream(data.getSource().getBytes())) {
-
-                parser.parse(new InputSource(sourceStream), new DefaultHandler() {
-                    // TODO: other handler methods
-                    @Override
-                    public void error(SAXParseException e) throws SAXException {
-                        result.setError(true);
-                        result.getErrors().add(e.getMessage());
-                    }
-                });
+                    parser.parse(new InputSource(sourceStream), new DefaultHandler() {
+                        // TODO: other handler methods
+                        @Override
+                        public void error(SAXParseException e) throws SAXException {
+                            result.setError(true);
+                            result.getErrors().add(e.getMessage());
+                        }
+                    });
+                }
             }
         }
-
-        res.setStatusCode(200);
         res.addHeader("Content-type", "application/json");
-        if(result.getErrors().size()>0) {
-            res.setStatusCode(200);
-        }
         res.setResponseText(mapper.writeValueAsString(result));
     }
 
