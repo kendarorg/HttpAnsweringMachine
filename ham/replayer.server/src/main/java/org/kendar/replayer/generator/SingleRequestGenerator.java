e76b8c396bee87d19a74005f3f3314074fbacc6a
Kendar <github@kendar.org>
2022-02-20 20:38:43 +0100

    Added generation api still missing zip file

--- ham/replayer.server/src/main/java/org/kendar/replayer/generator/SingleRequestGenerator.java
+++ ham/replayer.server/src/main/java/org/kendar/replayer/generator/SingleRequestGenerator.java
@@ -6,6 +6,7 @@ import org.kendar.replayer.storage.ReplayerRow;
 import org.kendar.servers.http.Request;
 import org.kendar.servers.http.RequestUtils;
 import org.kendar.utils.FileResourcesUtils;
+import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
@@ -13,6 +14,7 @@ import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 
+@Component
 public class SingleRequestGenerator {
     private FileResourcesUtils fileResourcesUtils;
 
