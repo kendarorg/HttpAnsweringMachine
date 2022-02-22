4ec6c04f6abdd5352641a03c48fabc7af113f799
Kendar <github@kendar.org>
2022-02-22 14:19:25 +0100

    Added replayer fields

--- ham/app/src/main/java/org/kendar/utils/FileResourcesUtilsImpl.java
+++ ham/app/src/main/java/org/kendar/utils/FileResourcesUtilsImpl.java
@@ -9,8 +9,13 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.*;
-import java.util.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
