b0a5a45a3c973d7778674511b2966ffb5805b0a0
Kendar <github@kendar.org>
2022-02-22 22:38:10 +0100

    Saving the simulator crap

--- ham/replayer.server/src/main/java/org/kendar/replayer/apis/models/ScriptData.java
+++ ham/replayer.server/src/main/java/org/kendar/replayer/apis/models/ScriptData.java
@@ -1,9 +1,15 @@
 package org.kendar.replayer.apis.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ScriptData {
     private String description;
     private String id;
     private RedirectFilter filter;
+    private List<Integer> stimulatorTest = new ArrayList<>();
+    private List<Integer> pactTest = new ArrayList<>();
+    private List<Integer> stimulatedTest = new ArrayList<>();
 
     public String getDescription() {
         return description;
@@ -27,5 +33,29 @@ public class ScriptData {
 
     public void setFilter(RedirectFilter filter) {
         this.filter = filter;
+    }
+
+    public List<Integer> getStimulatorTest() {
+        return stimulatorTest;
+    }
+
+    public void setStimulatorTest(List<Integer> stimulatorTest) {
+        this.stimulatorTest = stimulatorTest;
+    }
+
+    public List<Integer> getPactTest() {
+        return pactTest;
+    }
+
+    public void setPactTest(List<Integer> pactTest) {
+        this.pactTest = pactTest;
+    }
+
+    public List<Integer> getStimulatedTest() {
+        return stimulatedTest;
+    }
+
+    public void setStimulatedTest(List<Integer> stimulatedTest) {
+        this.stimulatedTest = stimulatedTest;
     }
 }
