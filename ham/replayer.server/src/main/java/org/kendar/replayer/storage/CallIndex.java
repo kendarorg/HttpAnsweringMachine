f4cfc0768fa7118c524f5123cf73852f411f2284
Kendar <github@kendar.org>
2022-02-22 14:02:15 +0100

    Added replayer fields

--- ham/replayer.server/src/main/java/org/kendar/replayer/storage/CallIndex.java
+++ ham/replayer.server/src/main/java/org/kendar/replayer/storage/CallIndex.java
@@ -4,6 +4,10 @@ public class CallIndex {
     private int id;
     private int reference;
     private String description;
+    private String preRequestJs;
+    private String middleJs;
+    private String postResponseJs;
+    private String testType;
 
     public int getId() {
         return id;
@@ -27,5 +31,37 @@ public class CallIndex {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public String getPreRequestJs() {
+        return preRequestJs;
+    }
+
+    public void setPreRequestJs(String preRequestJs) {
+        this.preRequestJs = preRequestJs;
+    }
+
+    public String getMiddleJs() {
+        return middleJs;
+    }
+
+    public void setMiddleJs(String middleJs) {
+        this.middleJs = middleJs;
+    }
+
+    public String getPostResponseJs() {
+        return postResponseJs;
+    }
+
+    public void setPostResponseJs(String postResponseJs) {
+        this.postResponseJs = postResponseJs;
+    }
+
+    public String getTestType() {
+        return testType;
+    }
+
+    public void setTestType(String testType) {
+        this.testType = testType;
     }
 }
--- ham/replayer.server/src/main/java/org/kendar/replayer/storage/ReplayerRow.java
+++ ham/replayer.server/src/main/java/org/kendar/replayer/storage/ReplayerRow.java
@@ -11,6 +11,10 @@ public class ReplayerRow {
     private Response response;
     private String requestHash;
     private String responseHash;
+    private String preRequestJs;
+    private String middleJs;
+    private String postResponseJs;
+    private String testType;
     private int id;
 
     public Calendar getTimestamp() {
@@ -68,5 +72,37 @@ public class ReplayerRow {
 
     public boolean done(){
         return done;
+    }
+
+    public String getPreRequestJs() {
+        return preRequestJs;
+    }
+
+    public void setPreRequestJs(String preRequestJs) {
+        this.preRequestJs = preRequestJs;
+    }
+
+    public String getMiddleJs() {
+        return middleJs;
+    }
+
+    public void setMiddleJs(String middleJs) {
+        this.middleJs = middleJs;
+    }
+
+    public String getPostResponseJs() {
+        return postResponseJs;
+    }
+
+    public void setPostResponseJs(String postResponseJs) {
+        this.postResponseJs = postResponseJs;
+    }
+
+    public String getTestType() {
+        return testType;
+    }
+
+    public void setTestType(String testType) {
+        this.testType = testType;
     }
 }
