b4fa563e8cb440e5aa8a07adaaf9793444cb5d60
Kendar <github@kendar.org>
2022-02-28 16:39:19 +0100

    refactor sampleapp

--- ham/app.commons/src/main/java/org/kendar/servers/http/InternalRequester.java
+++ ham/app.commons/src/main/java/org/kendar/servers/http/InternalRequester.java
@@ -1,5 +1,7 @@
 package org.kendar.servers.http;
 
 public interface InternalRequester extends BaseRequester{
+    void callSite(Request request, Response response)
+            throws Exception;
 
 }
