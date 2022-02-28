b2a5b21c610fba1b2d66de6ce07394bf85e661b0
Kendar <github@kendar.org>
2022-02-28 16:39:19 +0100

    refactor sampleapp

--- ham/app.commons/src/main/java/org/kendar/servers/http/ExternalRequester.java
+++ ham/app.commons/src/main/java/org/kendar/servers/http/ExternalRequester.java
@@ -1,5 +1,7 @@
 package org.kendar.servers.http;
 
 public interface ExternalRequester extends BaseRequester{
+    void callSite(Request request, Response response)
+            throws Exception;
 
 }
