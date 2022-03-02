816d7474bc677cbb42f3b1546a47c957f3f11fe2
Kendar <github@kendar.org>
2022-03-02 09:38:47 +0100

    Fix crlf and odd loggins

--- samples/sampleapp/docker_multi/fe.sh
+++ samples/sampleapp/docker_multi/fe.sh
@@ -4,4 +4,6 @@ export JAVA_HOME=/usr/lib/jvm/java-11-openjdk/
 export PATH="${JAVA_HOME}/bin:${PATH}"
 
 cd /etc/app/fe
-java -jar -Dserver.port=80 /etc/app/fe/fe-1.0-SNAPSHOT.jar
+java -jar -Dserver.port=80 \
+  -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=0.0.0.0:5005 \
+  /etc/app/fe/fe-1.0-SNAPSHOT.jar
