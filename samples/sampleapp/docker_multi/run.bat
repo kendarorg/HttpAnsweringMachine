8134b35a386c2bd879f95e40afb9f8f09aa1a6b0
Kendar <github@kendar.org>
2022-03-02 17:38:05 +0100

    Added execution end editing of js during pact testes

--- samples/sampleapp/docker_multi/run.bat
+++ samples/sampleapp/docker_multi/run.bat
@@ -1,6 +1,2 @@
-docker build -t ham.sampleapp.multi -f Dockerfile.master ..\
-docker build -t ham.sampleapp.fe -f Dockerfile.fe ..\
-docker build -t ham.sampleapp.be -f Dockerfile.be ..\
-docker build -t ham.sampleapp.be.users -f Dockerfile.be.users ..\
+
 docker-compose up
-pause
