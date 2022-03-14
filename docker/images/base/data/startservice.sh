91084a9f0bd0cdb5142286b0bfbb76f857cc7b8c
Kendar <github@kendar.org>
2022-03-14 10:06:37 +0100

    Finished refactoring

--- docker/images/base/data/startservice.sh
+++ docker/images/base/data/startservice.sh
@@ -9,6 +9,8 @@ mkdir -p $rootServicesDir
 captureLogs="false"
 isConfig="false"
 appDir="unknown"
+runner="unknown"
+runnerSet="false"
 for arg in "$@"
 do
     if [[ "$arg" == "--capturelogs" ]]; then
@@ -20,12 +22,17 @@ do
     if [[ $arg == --app=* ]]; then
       appDir=${arg:6}
     fi
+    if [[ $arg == --run=* ]]; then
+      runner=${arg:6}
+	  runnerSet="true"
+    fi
 done
 
 if [[ "$appDir" == "unknown" ]]; then
   echo ""
   echo "startservice.sh params:"
   echo "  --app=APPDIR, mandatory, run ${rootAppDir}/APPDIR/APPDIR.sh file"
+  echo "  --run=APPDIR, optional, sets the full path of the executable to run"
   echo "  --capturelogs, optional, write the logs in ${rootAppDir}//APPDIR/logs"
   echo "    instead of stdout"
   echo "  --config, optional, execute once then sleep infinity"
@@ -34,8 +41,15 @@ if [[ "$appDir" == "unknown" ]]; then
   exit 1
 fi
 
+
+
 echo Application Director $rootAppDir/$appDir
 
+if [[ $runnerSet == true ]]; then
+  echo Set alternative runner path $runner
+else
+  runner = $rootAppDir/$appDir/$appDir.sh
+fi
 if [[ $captureLogs == true ]]; then
   echo Capture Logs on $rootAppDir/$appDir/log
 fi
@@ -53,12 +67,13 @@ mkdir -p "${rootAppDir}/${appDir}"
 mkdir -p "${rootServicesDir}/${appDir}"
 echo '#!/bin/bash' > $rootServicesDir/$appDir/run
 echo "exec 2>&1" >> $rootServicesDir/$appDir/run
-echo "exec ${rootAppDir}/${appDir}/${appDir}.sh" >> $rootServicesDir/$appDir/run
+echo "exec ${runner}" >> $rootServicesDir/$appDir/run
 if [[ "$isConfig" == "true" ]]; then     ## GOOD
   echo "sleep infinity" >> $rootServicesDir/$appDir/run
 fi
 
 chmod +x "${rootServicesDir}/${appDir}/run"
+chmod +x "${runner}s"
 #chmod +x "${rootAppDir}/${appDir}/${appDir}.sh"
 
 if [ "$captureLogs" == "true" ]; then     ## GOOD
