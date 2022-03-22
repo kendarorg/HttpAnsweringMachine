7b0d15f2e42a6c839ce850b029cf006f92c9b4e6
Kendar <unknown@kendar.org>
2022-03-22 17:59:11 +0100

    Fixing pact replacements

--- Run.bat
+++ Run.bat
@@ -25,7 +25,7 @@ goto end
 
 :simple
 
-echo Open with notepad (administrative rights) 
+echo Open with notepad (administrative rights)
 echo the file C:\Windows\System32\drivers\etc\hosts
 echo and add the following lines:
 echo 127.0.0.1  www.local.test
@@ -35,35 +35,36 @@ echo 127.0.0.1  be.sample.test
 
 pause
 
-
-
-
-cd %mypath%samples\sampleapp\gateway\target
-copy /Y %mypath%samples\sampleapp\docker\application.properties.gateway %mypath%samples\sampleapp\gateway\target\application.properties
-start java -jar %mypath%samples\sampleapp\gateway\target\gateway-1.0-SNAPSHOT.jar
-
-cd %mypath%samples\sampleapp\be\target	
-copy /Y %mypath%samples\sampleapp\docker\application.properties.be %mypath%samples\sampleapp\be\target\application.properties
-start java -jar %mypath%samples\sampleapp\be\target\be-1.0-SNAPSHOT.jar
-	
-cd %mypath%samples\sampleapp\fe\target
-copy /Y %mypath%samples\sampleapp\docker\application.properties.fe %mypath%samples\sampleapp\fe\target\application.properties
-start java -jar %mypath%samples\sampleapp\fe\target\fe-1.0-SNAPSHOT.jar
-	
-	
 cd %mypath%ham\app\target
 
 md "%mypath%ham\app\target\libs" 2>NUL
 del /q %mypath%ham\app\target\libs\*.*
-copy /Y %mypath%\libs\*.jar %mypath%ham\app\target\libs\
+copy /Y %mypath%ham\libs\*.jar %mypath%ham\app\target\libs\
 
 copy /Y %mypath%samples\sampleapp\standalone.external.json %mypath%ham\app\target\external.json
 start java "-Dloader.path=%mypath%ham\app\target\libs"  -Dloader.main=org.kendar.Main  ^
   	-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=0.0.0.0:5025 ^
 	-jar app-1.0-SNAPSHOT.jar org.springframework.boot.loader.PropertiesLauncher
-	
-	
-	
+
+pause
+
+cd %mypath%samples\sampleapp\gateway\target
+copy /Y %mypath%samples\sampleapp\docker\application.properties.gateway %mypath%samples\sampleapp\gateway\target\application.properties
+start java -jar %mypath%samples\sampleapp\gateway\target\gateway-1.0-SNAPSHOT.jar
+
+pause
+
+cd %mypath%samples\sampleapp\be\target
+copy /Y %mypath%samples\sampleapp\docker\application.properties.be %mypath%samples\sampleapp\be\target\application.properties
+start java -jar %mypath%samples\sampleapp\be\target\be-1.0-SNAPSHOT.jar
+
+pause
+
+cd %mypath%samples\sampleapp\fe\target
+copy /Y %mypath%samples\sampleapp\docker\application.properties.fe %mypath%samples\sampleapp\fe\target\application.properties
+start java -jar %mypath%samples\sampleapp\fe\target\fe-1.0-SNAPSHOT.jar
+
+
 :end
 
 pause
