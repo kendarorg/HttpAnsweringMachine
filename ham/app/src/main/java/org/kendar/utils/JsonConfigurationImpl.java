758a0f3f805c8979dd5d71c23bfe1391789f99ac
Kendar <unknown@kendar.org>
2022-03-25 11:56:34 +0100

    Configuration includes

--- ham/app/src/main/java/org/kendar/utils/JsonConfigurationImpl.java
+++ ham/app/src/main/java/org/kendar/utils/JsonConfigurationImpl.java
@@ -12,12 +12,16 @@ import org.kendar.servers.config.ConfigAttribute;
 import org.slf4j.Logger;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashSet;
 import java.util.Locale;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Component
 public class JsonConfigurationImpl implements JsonConfiguration {
@@ -109,7 +113,10 @@ public class JsonConfigurationImpl implements JsonConfiguration {
   }
 
   public void loadConfiguration(String fileName) throws Exception {
-    var fullConfig = Files.readString(Path.of(fileName));
+    var filePath = Path.of(fileName);
+    var fullConfig = Files.readString(filePath);
+    var dirPath = filePath.toAbsolutePath().getParent();
+    fullConfig = handleIncludes(fullConfig,dirPath);
     var treeMap = mapper.readTree(fullConfig).elements();
     while (treeMap.hasNext()) {
       var pluginRoot = treeMap.next();
@@ -120,6 +127,39 @@ public class JsonConfigurationImpl implements JsonConfiguration {
       }
       configurations.put(id, pluginRoot);
     }
+  }
+
+  private String handleIncludes(String fullConfig, Path dirPath) throws IOException {
+    var regex = "(?m)[\"']#include:([0-9a-zA-Z_\\-\\.\\\\/]+)[\"']";
+    Pattern pattern = Pattern.compile(regex);
+    Matcher matcher = pattern.matcher(fullConfig);
+    var hasMatches = true;
+    while(hasMatches){
+      var hashSet = new HashSet<>();
+      var iterations = 0;
+      while(matcher.find()){
+        iterations++;
+        var group0 = matcher.group(0);
+        var group1 = matcher.group(1);
+        if(hashSet.contains(group0)){
+          continue;
+        }
+        hashSet.add(group0);
+        var includePath = Path.of(group1);
+        if(!includePath.isAbsolute()){
+          includePath = Path.of(dirPath.toString(),includePath.toString());
+
+        }
+        var includeFile = Files.readString(includePath);
+        fullConfig = fullConfig.replaceAll(Pattern.quote(group0),includeFile);
+      }
+      //Re-check the match for extra infos
+      if(iterations==0){
+        break;
+      }
+      matcher = pattern.matcher(fullConfig);
+    }
+    return fullConfig;
   }
 
   public void saveConfiguration(String fileName) throws Exception {
