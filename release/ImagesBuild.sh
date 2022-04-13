d1cb2ef96b9004489af74a74f2bd628b6072fb84
Enrico Da Ros <enrico.daros@lastminute.com>
2022-04-13 09:29:09 +0200

    Add release sh

---
+++ release/ImagesBuild.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+mypath=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/
+
+cd $mypath
+
+cd base
+docker build -t ham.base .
+
+
+cd ../client
+md "data/app" 2>NUL
+rm -f data\app\*.*
+cp -f ../simpledns*.jar data/
+docker build -t ham.client .
+
+cd ../proxy
+docker build -t ham.proxy .
+
+
+cd ../openvpn
+docker build -t ham.openvpn .
+
+#exit 0
+
+cd ../master
+mkdir -p data/app
+rm -rf data/app/*.*
+mkdir -p data/app/libs
+rm -rf data/app/libs/*.*
+cp -f ../app*.jar data/app/
+cp -f ../libs/*.jar data/app/libs/
+docker build -t ham.master .
+
+cd ../singlemaster
+docker build -t ham.singlemaster .
+
+cd ..
