68d37e8a9c519251631b64249ade9108e1215454
Enrico Da Ros <enrico.daros@lastminute.com>
2022-04-13 09:29:09 +0200

    Add release sh

---
+++ release/ImagesBuild.bat
@@ -0,0 +1,55 @@
+@echo off
+SET mypath=%~dp0
+cd %mypath%
+echo %mypath%
+cd base
+docker build -t ham.base .
+
+
+cd ../client
+md "data\app" 2>NUL
+del /q data\app\*.*
+copy /Y ..\simpledns*.jar data\
+docker build -t ham.client .
+
+cd ../proxy
+docker build -t ham.proxy .
+
+REM cd ..\externalvpn\forticlient
+REM copy /Y ..\..\base\data\startservice.sh data/
+REM copy /Y ..\..\base\data\sshd_config data/
+REM copy /Y ..\..\base\data\ca.crt data/
+REM echo Build Vpn/forticlient
+REM docker build -t ham.forticlient .
+REM cd ..\
+
+
+REM cd ..\externalvpn\openconnect
+REM copy /Y ..\..\base\data\startservice.sh data/
+REM copy /Y ..\..\base\data\sshd_config data/
+REM copy /Y ..\..\base\data\ca.crt data/
+REM echo Build Vpn/Openconnect
+REM docker build -t ham.openconnect .
+REM cd ..\
+
+
+cd ../openvpn
+docker build -t ham.openvpn .
+
+REM exit
+
+cd ../master
+md "data\app" 2>NUL
+del /q data\app\*.*
+md "data\app\libs" 2>NUL
+del /q data\app\libs\*.*
+copy /Y ..\app*.jar data\app\
+copy /Y ..\libs\*.jar data\app\libs\
+docker build -t ham.master .
+
+cd ../singlemaster
+docker build -t ham.singlemaster .
+
+:end
+cd ..
+pause
