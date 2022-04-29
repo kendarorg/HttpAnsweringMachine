9e29f605ca2f6270876bb6a97335025d8db7dc06
Kendar <unknown@kendar.org>
2022-04-29 13:09:41 +0200

    Standalone

--- RunCalendar.bat
+++ RunCalendar.bat
@@ -14,7 +14,6 @@ goto simple
 
 echo Please install OpenVpn connect (https://openvpn.net/client-connect-vpn-for-windows/)
 echo Or use socks5://localhost:1080 proxy
-call explorer https://openvpn.net/client-connect-vpn-for-windows/
 echo and import %mypath%docker\images\openvpn\mainuser.local.ovpn profile
 echo then after connecting you will have full access!
 pause
