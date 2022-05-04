49f3754b5f21f4176ccb368d982e2ddcd88e0561
Kendar <unknown@kendar.org>
2022-05-04 12:15:15 +0200

    Adding certificates

--- ham/api.ham/src/test/java/org/kendar/ham/ProxiesIT.java
+++ ham/api.ham/src/test/java/org/kendar/ham/ProxiesIT.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ProxiesIT {
     @Test
-    public void testAddingCertificate() throws HamException, InterruptedException {
+    public void testAddingProxy() throws HamException, InterruptedException {
         var proxyId = GlobalSettings.builder()
                 .proxies()
                 .addProxy("http://www.microsoft.com","http://www.local.test/api/health","www.local.test:80");
