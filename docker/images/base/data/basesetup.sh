85477b4e501137347fd860181c983786c51008a1
Kendar <unknown@kendar.org>
2022-05-09 15:30:39 +0200

    Debian

--- docker/images/base/data/basesetup.sh
+++ docker/images/base/data/basesetup.sh
@@ -15,10 +15,23 @@ mkdir -p /root/.ssh
 mkdir -p /run/sshd
 chmod 0700 /root/.ssh
 
+# Create a "nice" sleep
+cd /etc/
+${JAVA11_HOME}/bin/javac DoSleep.java
+echo "#"'!'"/bin/bash
+cd /etc
+${JAVA11_HOME}/bin/java DoSleep" > /etc/DoSleep.sh
+
+
+chmod +x /etc/DoSleep.sh
+
 # Start sshd
 /etc/startservice.sh --app=sshd --capturelogs --run=/usr/sbin/sshd
 
 # Force root password
 mkdir -p /etc/service/rootforce 
-echo -e "#"'!'"/bin/bash\nexec 2>&1\necho \"root:\${ROOT_PWD}\"|chpasswd\nsleep infinity\n" > /etc/service/rootforce/run
+echo "#"'!'"/bin/bash
+exec 2>&1
+echo \"root:\${ROOT_PWD}\"|chpasswd
+/etc/DoSleep.sh" > /etc/service/rootforce/run
 chmod +x /etc/service/rootforce/run
--- docker/images/client/data/simpledns.sh
+++ docker/images/client/data/simpledns.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-export JAVA_HOME=/usr/lib/jvm/java-11-openjdk/
+export JAVA_HOME=${JAVA11_HOME}/
 export PATH="${JAVA_HOME}/bin:${PATH}"
+
 dnsServer=`ping -c 4 $DNS_HIJACK_SERVER|head -n 1| grep -Eo "([0-9]+\.?){4}"`
 
 
-echo "namserver 127.0.0.1" > /etc/resolv.conf
-echo "namserver 127.0.0.11" >> /etc/resolv.conf
-echo "options ndots:0" >> /etc/resolv.conf
+echo "namserver 127.0.0.1" > $RESOL_CONF
+echo "namserver 127.0.0.11" >> $RESOL_CONF
+echo "options ndots:0" >> $RESOL_CONF
 cd /etc/app/simpledns/
+
+#POSTRESOLVCONF
 
 ls -lA | awk -F':[0-9]* ' '/:/{print $2}'|grep .jar$ > tmp_txt
 export JAR_NAME=$(head -1 tmp_txt)
