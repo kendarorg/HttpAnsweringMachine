b19ba072bc5ede5253b0dcbbaae3c6fe38791aee
Kendar <unknown@kendar.org>
2022-05-10 23:23:59 +0200

    First time ever i should have used rebase

--- samples/quotes/docker_multi/ImagesBuild.bat
+++ samples/quotes/docker_multi/ImagesBuild.bat
@@ -1,5 +1,3 @@
 @echo off
-docker build -t ham.sampleapp.multi -f Dockerfile.master ..\
-docker build -t ham.sampleapp.fe -f Dockerfile.fe ..\
-docker build -t ham.sampleapp.be -f Dockerfile.be ..\
-docker build -t ham.sampleapp.gateway -f Dockerfile.gateway ..\
+docker build -t ham.quotes.master -f master.Dockerfile ..\
+docker build -t ham.quotes.core -f core.Dockerfile ..\
