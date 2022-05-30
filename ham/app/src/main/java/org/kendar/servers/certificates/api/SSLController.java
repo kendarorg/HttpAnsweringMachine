f4d173dbfd1e4ae6e521ae6f1e78e15264f18a4e
kendarorg <kendarorg@users.noreply.github.com>
2022-05-30 10:04:42 +0200

    82 site certificate generator csr (#86)
    * Adding ssl generation from UI

--- ham/app/src/main/java/org/kendar/servers/certificates/api/SSLController.java
+++ ham/app/src/main/java/org/kendar/servers/certificates/api/SSLController.java
@@ -2,6 +2,7 @@ package org.kendar.servers.certificates.api;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.codec.binary.Base64;
 import org.kendar.events.EventQueue;
 import org.kendar.events.events.SSLChangedEvent;
 import org.kendar.http.FilteringClass;
@@ -9,10 +10,15 @@ import org.kendar.http.HttpFilterType;
 import org.kendar.http.annotations.HttpMethodFilter;
 import org.kendar.http.annotations.HttpTypeFilter;
 import org.kendar.servers.JsonConfiguration;
+import org.kendar.servers.certificates.CertificatesManager;
+import org.kendar.servers.certificates.GeneratedCert;
+import org.kendar.servers.certificates.api.models.TLSSSLGenerator;
 import org.kendar.servers.config.SSLConfig;
 import org.kendar.servers.config.SSLDomain;
 import org.kendar.servers.http.Request;
 import org.kendar.servers.http.Response;
+import org.kendar.utils.FileResourcesUtils;
+import org.kendar.utils.LoggerBuilder;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -26,12 +32,21 @@ public class SSLController implements FilteringClass {
   final ObjectMapper mapper = new ObjectMapper();
   private final JsonConfiguration configuration;
   private final EventQueue eventQueue;
+  private final CertificatesManager certificatesManager;
+  private final LoggerBuilder loggerBuilder;
+  private final FileResourcesUtils fileResourcesUtils;
 
   public SSLController(JsonConfiguration configuration,
-                       EventQueue eventQueue) {
+                       EventQueue eventQueue,
+                       CertificatesManager certificatesManager,
+                       LoggerBuilder loggerBuilder,
+                       FileResourcesUtils fileResourcesUtils) {
 
     this.configuration = configuration;
     this.eventQueue = eventQueue;
+    this.certificatesManager = certificatesManager;
+    this.loggerBuilder = loggerBuilder;
+    this.fileResourcesUtils = fileResourcesUtils;
   }
 
   @Override
@@ -74,10 +89,10 @@ public class SSLController implements FilteringClass {
   }
 
   @HttpMethodFilter(
-      phase = HttpFilterType.API,
-      pathAddress = "/api/ssl",
-      method = "POST",
-      id = "1011a4b4-2ASD77d-11ec-9621-0242ac130002")
+          phase = HttpFilterType.API,
+          pathAddress = "/api/ssl",
+          method = "POST",
+          id = "1011a4b4-2ASD77d-11ec-9621-0242ac130002")
   public void addDnsServer(Request req, Response res) throws Exception {
     var cloned = configuration.getConfiguration(SSLConfig.class).copy();
 
@@ -98,12 +113,36 @@ public class SSLController implements FilteringClass {
     }
     final var newList2= newList;
     var notNew = cloned.getDomains().stream()
-                    .filter(a-> newList2.stream().noneMatch(m->m.getAddress().equalsIgnoreCase(a.getAddress())))
+            .filter(a-> newList2.stream().noneMatch(m->m.getAddress().equalsIgnoreCase(a.getAddress())))
             .collect(Collectors.toList());
     newList.addAll(notNew);
     cloned.setDomains(newList);
     configuration.setConfiguration(cloned);
     res.setStatusCode(200);
     eventQueue.handle(new SSLChangedEvent());
+  }
+
+  //TODO https://gist.github.com/fntlnz/cf14feb5a46b2eda428e000157447309
+  @HttpMethodFilter(
+          phase = HttpFilterType.API,
+          pathAddress = "/api/sslgen",
+          method = "POST",
+          id = "1011a4b4-asdfD77d-11ec-9621-0242ac130002")
+  public void generateSSL(Request req, Response res) throws Exception {
+
+    var request = mapper.readValue(req.getRequestText(), TLSSSLGenerator.class);
+
+    var root = certificatesManager.loadRootCertificate("certificates/ca.der", "certificates/ca.key");
+    GeneratedCert domain =
+            certificatesManager.createCertificate(
+                    request.getCn(), null, root, request.getExtraDomains(), false);
+    var encodedBytes = domain.certificate.getEncoded();
+    //final FileOutputStream os = new FileOutputStream("target/local.gamlor.info.cer");
+    String result = "-----BEGIN CERTIFICATE-----\n";
+    result+=new String(Base64.encodeBase64(encodedBytes, true));
+    result+="-----END CERTIFICATE-----\n";
+    res.setResponseText(result);
+    res.addHeader("content-type","application/pkix-cert");
+    res.setStatusCode(200);
   }
 }
