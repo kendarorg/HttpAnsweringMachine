731dd592768f75eb01be9496592a41e952d0bc83
Kendar <unknown@kendar.org>
2022-06-20 15:40:11 +0200

    Added custom annotations

---
+++ ham/app.http/src/main/java/org/kendar/http/annotations/multi/HamResponse.java
@@ -0,0 +1,11 @@
+package org.kendar.http.annotations.multi;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface HamResponse {
+    String content() default "application/json";
+    int code() default 200;
+    Class<?> body() default Object.class;
+}
