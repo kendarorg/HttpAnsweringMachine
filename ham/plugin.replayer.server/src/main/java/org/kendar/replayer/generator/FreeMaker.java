dd79f99cc306a11580803ada9e537a085738fd27
Enrico Da Ros <enrico.daros@lastminute.com>
2022-06-27 08:17:21 +0200

    Starting test

---
+++ ham/plugin.replayer.server/src/main/java/org/kendar/replayer/generator/FreeMaker.java
@@ -0,0 +1,8 @@
+package org.kendar.replayer.generator;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class FreeMaker {
+
+}
---
+++ ham/plugin.replayer.server/src/test/java/org/kendar/freemaker/Car.java
@@ -0,0 +1,32 @@
+package org.kendar.freemaker;
+
+public class Car {
+
+    private String name;
+    private int price;
+
+    public Car() {
+    }
+
+    public Car(String name, int price) {
+
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
