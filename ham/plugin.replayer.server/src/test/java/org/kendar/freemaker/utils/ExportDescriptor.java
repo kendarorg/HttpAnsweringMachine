e215237783fcd51db2578640aa089be98df356b5
Enrico Da Ros <enrico.daros@lastminute.com>
2022-06-27 09:33:55 +0200

    Starting test

---
+++ ham/plugin.replayer.server/src/test/java/org/kendar/freemaker/utils/ExportDescriptor.java
@@ -0,0 +1,9 @@
+package org.kendar.freemaker.utils;
+
+import java.util.List;
+
+public class ExportDescriptor {
+    public String name;
+    public String mainPackage;
+    public List<FileDescriptor> files;
+}
---
+++ ham/plugin.replayer.server/src/test/java/org/kendar/freemaker/utils/FileDescriptor.java
@@ -0,0 +1,10 @@
+package org.kendar.freemaker.utils;
+
+import java.util.List;
+
+public class FileDescriptor {
+    public byte[] content;
+    public List<String> path;
+    public String name;
+    public String ext;
+}
