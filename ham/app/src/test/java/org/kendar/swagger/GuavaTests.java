fc1bea1e72e538a985c9a5c91f72a2c6ff585986
Kendar <unknown@kendar.org>
2022-07-01 10:42:03 +0200

    Moving to 3.0.7-SNAPSHOT

--- ham/app/src/test/java/org/kendar/swagger/GuavaTests.java
+++ ham/app/src/test/java/org/kendar/swagger/GuavaTests.java
@@ -3,13 +3,14 @@ package org.kendar.swagger;
 import com.google.common.primitives.Primitives;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class GuavaTests {
     @Test
     public void test(){
         assertTrue(Primitives.isWrapperType(Integer.class));
-        assertTrue(Primitives.isWrapperType(int.class));
+        assertFalse(Primitives.isWrapperType(int.class));
         assertTrue(int.class.isPrimitive());
         assertTrue(Primitives.unwrap(Integer.class).isPrimitive());
         assertTrue(int[].class.isArray());
