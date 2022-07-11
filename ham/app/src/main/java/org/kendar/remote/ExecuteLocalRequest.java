b6157ceb2263972c77ac473dda377d83fa5fce49
Enrico Da Ros <enrico.daros@lastminute.com>
2022-07-11 09:16:50 +0200

    Adding simple Rest Client like API

---
+++ ham/app/src/main/java/org/kendar/remote/ExecuteLocalRequest.java
@@ -0,0 +1,16 @@
+package org.kendar.remote;
+
+import org.kendar.events.Event;
+import org.kendar.servers.http.Request;
+
+public class ExecuteLocalRequest implements Event {
+    private Request request;
+
+    public void setRequest(Request request) {
+        this.request = request;
+    }
+
+    public Request getRequest() {
+        return request;
+    }
+}
