2aa7f5dec40e501cda6e5d6f3d2033dd1fc8a0b3
Enrico Da Ros <enrico.daros@lastminute.com>
2022-09-23 12:31:56 +0200

    Updated scripts to build and run base app

---
+++ scripts/libs/docker.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+export DOCKER_USERNAME=none
+export DOCKER_LOGIN=none
+export DOCKER_TOKEN=none
+export DOCKER_ORG=none
+
+docker_login(){
+  DOCKER_USERNAME=$1
+  DOCKER_PASSWORD=$2
+  DOCKER_ORG=$3
+  docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+  DOCKER_TOKEN=`curl -s -H "Content-Type: application/json" -X POST -d "$(_docker_login_data)" "https://hub.docker.com/v2/users/login/" | jq -r .token`
+  DOCKER_PASSWORD=none
+}
+
+docker_logout(){
+    DOCKER_PASSWORD=none
+    DOCKER_TOKEN=none
+}
+
+_docker_login_data() {
+cat <<EOF
+{
+  "username": "$DOCKER_USERNAME",
+  "password": "$DOCKER_USERNAME"
+}
+EOF
+}
+
+docker_remove_tag () {
+  IMAGE_NAME=$2
+  TAG=$3
+  curl "https://hub.docker.com/v2/repositories/${DOCKER_ORG}/${IMAGE_NAME}/tags/${TAG}/" \
+  -X DELETE \
+  -H "Authorization: JWT ${DOCKER_TOKEN}"
+}
+
+docker_push(){
+  IMAGE_NAME=$1
+  VERSION_NUMBER=$2
+  
+  if [[ "$VERSION_NUMBER" == *"snapshot"* ]] ;then
+    echo Removing tag $IMAGE_NAME
+    docker_remove_tag "$IMAGE_NAME" snapshot
+    docker_remove_tag "$IMAGE_NAME" "$VERSION_NUMBER"
+    echo Pushing image tag $IMAGE_NAME
+    docker push $DOCKER_ORG/%IMAGE_NAME%:v%VERSION_NUMBER%
+    docker push $DOCKER_ORG/%IMAGE_NAME%:snapshot
+    echo Tagging image $IMAGE_NAME
+    docker tag %IMAGE_NAME% $DOCKER_ORG/%IMAGE_NAME%:v%VERSION_NUMBER%
+    docker tag $DOCKER_ORG/%IMAGE_NAME%:v%VERSION_NUMBER%-SNAPSHOT $DOCKER_ORG/%IMAGE_NAME%:snapshot
+  else
+    echo Pushing image tag $IMAGE_NAME
+    docker push $DOCKER_ORG/$IMAGE_NAME:v$VERSION_NUMBER
+    docker push $DOCKER_ORG/$IMAGE_NAME:latest
+    docker push $DOCKER_ORG/$IMAGE_NAME:snapshot
+    echo Tagging image $IMAGE_NAME
+    docker tag $IMAGE_NAME $DOCKER_ORG/$IMAGE_NAME:v$VERSION_NUMBER
+    docker tag $DOCKER_ORG/$IMAGE_NAME:v$VERSION_NUMBER $DOCKER_ORG/$IMAGE_NAME:latest
+    docker tag $DOCKER_ORG/$IMAGE_NAME:v$VERSION_NUMBER $DOCKER_ORG/$IMAGE_NAME:snapshot
+  fi
+}
--- scripts/libs/utils.sh
+++ scripts/libs/utils.sh
@@ -4,3 +4,11 @@ function pause(){
  read -s -n 1 -p "Press any key to continue . . ."
  echo ""
 }
+
+function read_password(){
+  stty_orig=$(stty -g) # save original terminal setting.
+  stty -echo           # turn-off echoing.
+  IFS= read -r passwd  # read the password
+  stty "$stty_orig"    # restore terminal setting.
+  echo $passwd
+}
