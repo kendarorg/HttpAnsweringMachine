a09dd9de62454b2f275f16e54bbbe56e88e5f4b9
Kendar <unknown@kendar.org>
2022-09-28 15:30:26 +0200

    Review of batches

---
+++ scripts/deploy_jar.bat
@@ -0,0 +1,25 @@
+@echo off
+
+REM Initialize
+set START_LOCATION=%cd%
+set SCRIPT_DIR=%~dp0
+
+REM Includes
+call %SCRIPT_DIR%\libs\version.bat
+set UTILS_LIB=%SCRIPT_DIR%\libs\utils.bat
+
+echo This will publish jars on the kendar maven repo. Ctrl+C to exit
+echo Target version: %HAM_VERSION%
+
+pause
+
+REM Extra initializations
+call %UTILS_LIB% set_parent_dir %SCRIPT_DIR% ROOT_DIR
+
+REM Deploys all jars on kendar mvn
+cd %ROOT_DIR%\ham
+echo Deploying ham
+call mvn deploy
+
+REM Restore previous dir
+cd %START_LOCATION%
