edf4906f5417ecc99654079ccdf8f72dc72e5d75
Kendar <unknown@kendar.org>
2022-09-29 17:46:23 +0200

    Clean scripts

---
+++ scripts/build/clean.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# Initialize
+START_LOCATION=$(pwd)
+. $(dirname "$0")/init.sh
+
+# Includes
+. $SCRIPT_DIR/libs/version.sh
+. $SCRIPT_DIR/libs/utils.sh
+
+echo This will build a tar.gz to run the application. Ctrl+C to exit
+echo Target version: $HAM_VERSION
+
+pause
+
+# Extra initializations
+ROOT_DIR=$( cd -- "$( dirname -- "$SCRIPT_DIR" )" &> /dev/null && pwd )
+
+# Setup the target directory
+echo Setup target dir
+HAM_RELEASE_TARGET=$ROOT_DIR/release/$HAM_VERSION
+rm -rf $HAM_RELEASE_TARGET || true
+mkdir -p $HAM_RELEASE_TARGET
+
+# Build HAM
+cd $ROOT_DIR/ham
+echo Building ham
+mvn clean
+rm -rf $ROOT_DIR/ham/jsplugins
+rm -rf $ROOT_DIR/ham/libs
+
+# Build samples
+cd $ROOT_DIR/samples/calendar
+echo Cleaning samples
+call mvn clean
+
+rm -rf $ROOT_DIR/release
+
+REM Restore previous dir
+cd %START_LOCATION%
