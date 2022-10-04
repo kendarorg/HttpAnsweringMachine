0f5557ef313873c4bc707dbf1185ff8d96616021
Kendar <unknown@kendar.org>
2022-10-04 11:27:46 +0200

    Autogenerating docs

--- docs/gen_scripts/nullinfrastructure.bat
+++ docs/gen_scripts/nullinfrastructure.bat
@@ -5,7 +5,8 @@ cd %~dp0
 set TARGET=..\generated
 set SOURCE=..\gen_sources
 
-echo In this demo you will > %TARGET%\nullinfrastracture.md
+type noedit.htm > %TARGET%\nullinfrastracture.md
+echo In this demo you will >> %TARGET%\nullinfrastracture.md
 type nullinfrastracture.md >> %TARGET%\nullinfrastracture.md
 type %SOURCE%\quickinstall.md >> %TARGET%\nullinfrastracture.md
 type %SOURCE%\proxy.md >> %TARGET%\localsample.md
