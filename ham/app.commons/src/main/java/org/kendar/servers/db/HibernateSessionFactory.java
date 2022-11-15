1b4463e36947e211250201636eeb8cd121a386b1
Kendar <unknown@kendar.org>
2022-11-15 11:42:42 +0100

    Config fixes

--- ham/app.commons/src/main/java/org/kendar/servers/db/HibernateSessionFactory.java
+++ ham/app.commons/src/main/java/org/kendar/servers/db/HibernateSessionFactory.java
@@ -8,10 +8,8 @@ import org.hibernate.cfg.Configuration;
 public interface HibernateSessionFactory {
     SessionFactory createSession() throws HibernateException;
     <T> T transactionalResult(EntityManagerFunctionResult function) throws Exception;
-
     <T> T queryResult(EntityManagerFunctionResult function) throws Exception;
     void transactional(EntityManagerFunction function) throws Exception;
-
     void query(EntityManagerFunction function) throws Exception;
 
     void setConfiguration(Configuration configuration);
