f3d5797d2269580371253822cf9a27996d2ccd79
Kendar <unknown@kendar.org>
2022-11-28 13:28:50 +0100

    Start testing

---
+++ ham/app/src/main/resources/web/vcomponents/test.vue
@@ -0,0 +1,14 @@
+<template>
+    <button v-on:click="count++">You clicked me {{ count }} times.</button>
+</template>
+<script>
+module.exports = {
+    data: function() {
+        return {
+            count: 0
+        }
+    }
+}
+</script>
+<style>
+</style>
