207b8c016568625481cedb1389aae857bf975024
Kendar <unknown@kendar.org>
2022-11-29 13:55:28 +0100

    bool checkbox

---
+++ ham/app/src/main/resources/web/vcomponents/tbool.vue
@@ -0,0 +1,11 @@
+<template>
+  <input type="checkbox" id="checkbox" v-model="data" disabled="true"/>
+</template>
+<script>
+module.exports = {
+  props: {
+    data: Boolean
+  },
+  name: 'tstring'
+}
+</script>
