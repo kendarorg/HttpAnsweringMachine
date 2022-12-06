63db0cd81adeb4c23a3405bb1c74c0c861a42738
Kendar <unknown@kendar.org>
2022-12-06 09:00:29 +0100

    Running DNS servers

---
+++ ham/app/src/main/resources/web/dns/vcomponents/vdnsmappings.vue
@@ -0,0 +1,103 @@
+<template>
+<div>
+  <simple-modal v-if="modalShow"
+                :modal-data="modalData"
+                @close="modalShow = false">
+    <span slot="header">Dns Mapping</span>
+    <span slot="body"><change-dns-mapping :data="modalData.data"/></span>
+  </simple-modal>
+  <h3>DNS Mappings</h3>
+  <button v-on:click="reload()" class="btn btn-primary btn-sm">Reload</button>
+  <button v-on:click="addNew(false,[])" class="btn btn-primary btn-sm">Add new</button>
+  <simple-grid
+      v-on:gridclicked="gridClicked"
+      ref="grid"
+      :columns="columns"
+      :extra="extraColumns"
+      :retrieve-data="retrieveData"
+  >
+  </simple-grid>
+</div>
+</template>
+<script>
+module.exports = {
+  components: {
+    'simple-grid': httpVueLoader('/vcomponents/testgrid.vue'),
+    'simple-modal': httpVueLoader('/vcomponents/tmodal.vue'),
+    'change-dns-mapping': httpVueLoader('/dns/vcomponents/veditdnsmapping.vue')
+  },
+  name: 'dns-mappings',
+  data: function () {
+    return {
+      modalData: null,
+      modalShow: false,
+      columns: [
+        {id: "id", template: "string", index: true},
+        {id: "ip", template: "string"},
+        {id: "dns", template: "string"}
+      ],
+      extraColumns: [
+        {
+          id: "_edit", template: "button", default: false, searchable: false, sortable: false, properties: {
+            name: "Edit", style: "btn btn-success btn-sm"
+          }
+        },
+        {
+          id: "_delete", template: "button", default: false, searchable: false, sortable: false, properties: {
+            name: "Delete", style: "btn btn-danger btn-sm"
+          }
+        }
+      ]
+    }
+  },
+  methods: {
+    retrieveData: async function () {
+      var result = await axios.get("/api/dns/mappings");
+      return result;
+    },
+    gridClicked: async function (evt) {
+      var row = this.$refs.grid.getById(evt.index);
+
+      if (evt.buttonid == "_edit") {
+        this.addNew(true, evt.index)
+      } else if (evt.buttonid == "_delete") {
+        await axios.delete("/api/dns/mappings/" + row['id'])
+        this.reload();
+      }
+    },
+    reload: function () {
+      this.$refs.grid.reload();
+    },
+    addNew: function (shouldEdit, rowId) {
+      var row = null;
+      if (shouldEdit) {
+        row = this.$refs.grid.getById(rowId);
+      } else {
+        row = {
+          id: URL.createObjectURL(new Blob([])).substr(-36),
+          ip:"127.0.0.1"
+        }
+      }
+      this.modalData = {
+        data: row,
+        edit: shouldEdit,
+        save: this.save
+      };
+      this.modalShow = true;
+    },
+    save: async function () {
+      if (this.modalData.edit) {
+        await axios.put('/api/dns/mappings/' + this.modalData.data.id, this.modalData.data).then(() => {
+          this.modalShow = false;
+          this.reload();
+        });
+      } else {
+        await axios.post('/api/dns/mappings', this.modalData.data).then(() => {
+          this.modalShow = false;
+          this.reload();
+        });
+      }
+    }
+  }
+}
+</script>
