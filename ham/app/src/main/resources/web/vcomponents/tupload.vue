3c639a8450f09139c4c29632b9373b0f97d065dc
Kendar <unknown@kendar.org>
2022-12-07 14:59:03 +0100

    Added upload component

---
+++ ham/app/src/main/resources/web/vcomponents/tupload.vue
@@ -0,0 +1,128 @@
+<template>
+  <div>
+    <div class="form-group">
+      <button type="button" id="btLoadFile" name="btLoadFile" class="btn btn-default" v-on:click="openFile">Open file</button>
+      <input ref="uploadScript" type="file" style="width:0;height: 0;opacity:0;"
+             id="uploadScript" name="uploadScript"
+             @change="loadFile"
+      >
+    </div>
+    <div class="form-group">
+      <input class="form-control" type="text" name="uploadScriptShow" id="uploadScriptShow"
+             v-model="uploadScriptShow"/>
+    </div>
+    <div class="form-group">
+      <button type="button" id="btUploadScript" name="btUploadScript" class="btn btn-default" v-on:click="upload">Upload</button>
+    </div>
+  </div>
+</template>
+<script>
+module.exports = {
+  props: {
+    useFormData:Boolean,
+    contentType:String,
+    path:String
+  },
+  name: 'ham-upload',
+  data:function(){
+    return {
+      scriptName: "",
+      fileContent:null,
+      uploadScriptShow:""
+    };
+  },
+  methods:{
+    openFile:function(){
+      this.fileContent = null;
+      this.$refs.uploadScript.click();
+    },
+    loadFile:function(){
+      var th = this;
+      this.uploadAsyncFile(this.$refs.uploadScript.files,function(data){
+        th.fileContent = data[0];
+        th.uploadScriptShow = data[0].name;
+      },function(exception){
+        this.$emit('error', {
+          files : [],
+          error : exception
+        });
+      });
+    },
+    upload:function(){
+      if(this.contentType==null||typeof this.contentType =="undefined"||this.contentType==""){
+        this.contentType="application/json";
+      }
+      if(this.useFormData==null||typeof this.useFormData == "undefined"||this.useFormData==false) {
+        var toUpload = JSON.stringify(this.fileContent);
+        const headers = {'Content-Type': this.contentType};
+        var th=this;
+        axios.post(this.path, toUpload, {headers}).then((res) => {
+          location.href = "script.html?id=" + res;
+          th.$emit('success',{
+            response:res,
+            name:th.uploadScriptShow,
+            length:toUpload.length
+          })
+        });
+      }else {
+        const formData = new FormData();
+        formData.append('file', this.fileContent);
+        const headers = { 'Content-Type': 'multipart/form-data' };
+        axios.post(this.path, formData, { headers }).
+        then((res) => {
+          th.$emit('success',{
+            response:res,
+            name:th.uploadScriptShow,
+            length:toUpload.length
+          });
+        });
+      }
+    },
+    splitOnFirst : function (str, sep) {
+      const index = str.indexOf(sep);
+      return index < 0 ? [str] : [str.slice(0, index), str.slice(index + sep.length)];
+    },
+    uploadAsyncFile : async function (files, callback, callbackError) {
+
+      let filesLoadedEvent = [];
+      let filesLoaded = [];
+      if (files && files.length) {
+        try {
+          for (let i = 0; i < files.length; i++) {
+            const uploadedImageBase64 = await this.convertFileToBase64(files[i], callback);
+            filesLoaded.push({
+              data: this.splitOnFirst(uploadedImageBase64, ",")[1],
+              name: files[i].name,
+              type: files[i].type
+            });
+            filesLoadedEvent.push({
+              name: files[i].name,
+              type: files[i].type
+            });
+          }
+
+
+        } catch (exception) {
+          this.$emit('error', {
+            files : filesLoadedEvent,
+            error : exception
+          });
+        }
+      } else {
+        this.$emit('error', {
+          files:[],
+          error:"No files to upldad"
+        });
+      }
+    },
+    convertFileToBase64 : function (file) {
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = reject;
+      });
+    }
+  }
+}
+</script>
--- ham/plugin.replayer.server/src/main/resources/web/plugins/recording/vcomponents/createrecording.vue
+++ ham/plugin.replayer.server/src/main/resources/web/plugins/recording/vcomponents/createrecording.vue
@@ -3,7 +3,12 @@
   <div class="row">
     <div class="col-md-8">
       <h3>UPLOAD SCRIPT</h3>
-      <form action="" id="addUser"  method="POST"  >
+        <ham-upload
+            path="/api/plugins/replayer/recording"
+            @success="onSuccess"
+            @error="onError"
+        ></ham-upload>
+      <!--<form action="" id="addUser"  method="POST"  >
         <div class="form-group">
           <button type="button" id="btLoadFile" name="btLoadFile" class="btn btn-default" v-on:click="openFile">Open file</button>
           <input ref="uploadScript" type="file" style="width:0;height: 0;opacity:0;"
@@ -18,7 +23,7 @@
         <div class="form-group">
           <button type="button" id="btUploadScript" name="btUploadScript" class="btn btn-default" v-on:click="upload">Upload</button>
         </div>
-      </form>
+      </form>-->
     </div>
     <div class="col-md-8">
       <h3>CREATE RECORDING</h3>
@@ -42,43 +47,18 @@ module.exports = {
   },
   data: function() {
     return {
-      scriptName: "",
-      fileContent:null,
-      uploadScriptShow:""
+
     }
   },
+  components: {
+    'ham-upload': httpVueLoader('/vcomponents/tupload.vue')
+  },
   methods:{
-    openFile:function(){
-      this.fileContent = null;
-      this.$refs.uploadScript.click();
+    onSuccess:function(data){
+      location.href = "script.html?id="+data.response.data;
     },
-    loadFile:function(){
-      var th = this;
-      uploadAsyncFile(this.$refs.uploadScript.files,function(data){
-        th.fileContent = data[0];
-        th.uploadScriptShow = data[0].name;
-      },function(exception){
-        alert(exception);
-      });
-    },
-    upload:function(){
-      var toUpload = JSON.stringify(this.fileContent);
-      var name = this.uploadScriptShow.substr(0,this.uploadScriptShow.length-5);
-      const headers = { 'Content-Type': 'application/json' };
-      axios.post('/api/plugins/replayer/recording', toUpload, { headers }).
-      then((res) => {
-        location.href = "script.html?id="+response;
-      });
-      /*const formData = new FormData();
-      formData.append('file', this.fileContent);
-      const headers = { 'Content-Type': 'multipart/form-data' };
-      axios.post('/api/plugins/replayer/recording', formData, { headers }).
-      then((res) => {
-        console.log(res)
-        res.data.files; // binary representation of the file
-        res.status; // HTTP status
-      });*/
-
+    onError:function(data){
+      alert(data.error);
     },
     create:function(){}
   }
