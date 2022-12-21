10b806fda073bcf9fe3323dd3e3046c08c375cc9
Kendar <unknown@kendar.org>
2022-12-21 13:18:59 +0100

    Tree thing

---
+++ ham/app/src/main/resources/web/plugins/recording/vcomponents/line/root.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+  </div>
+</template>
+<script>
+module.exports = {
+  name: "root",
+  props:{
+    value:Object
+  }
+}
+</script>
---
+++ ham/app/src/main/resources/web/plugins/recording/vcomponents/utils.js
@@ -0,0 +1,7 @@
+const orgkendarjanuscmdexecparamtype_togrid =function(data){
+
+}
+
+const orgkendarjanuscmdexecparamtype_fromgrid =function(data){
+
+}
