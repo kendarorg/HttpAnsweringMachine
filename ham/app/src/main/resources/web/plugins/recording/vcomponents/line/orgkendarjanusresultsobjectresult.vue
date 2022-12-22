13a28fb4fb4793a85f87af926e9744163565920d
Kendar <unknown@kendar.org>
2022-12-22 14:23:16 +0100

    ObjectResult

---
+++ ham/app/src/main/resources/web/plugins/recording/vcomponents/line/orgkendarjanusresultsobjectresult.vue
@@ -0,0 +1,48 @@
+<template>
+  <div v-if="value.type=='org.kendar.janus.results.ObjectResult'">
+
+    <b>{{ value.type }}</b><br>
+    <div class="form-group">
+      <label htmlFor="typ">Result Type</label>
+      <input class="form-control" type="text" name="typ" id="typ" v-model="otype"/>
+    </div>
+    <div class="form-group">
+      <label htmlFor="val">Value</label>
+      <input class="form-control" type="text" name="val" id="val" v-model="ovalue"/>
+    </div>
+  </div>
+</template>
+<script>
+module.exports = {
+  name: "orgkendarjanusresultsobjectresult",
+  props: {
+    value: Object
+  },
+  computed:{
+    otype: {
+      get: function () {
+        return this.value.children[0].type;
+      },
+      // setter
+      set: function (newValue) {
+        this.value.children[0].type=newValue;
+        this.$emit("componentevent",{
+          id:"changed"
+        })
+      }
+    },
+    ovalue: {
+      get: function () {
+        return this.value.children[0].value;
+      },
+      // setter
+      set: function (newValue) {
+        this.value.children[0].value=newValue;
+        this.$emit("componentevent",{
+          id:"changed"
+        })
+      }
+    }
+  }
+}
+</script>
