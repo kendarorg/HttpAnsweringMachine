43b7c25f6f1bc9f195ebd87f18f7a5d6b0f1a599
Kendar <unknown@kendar.org>
2022-12-29 15:05:21 +0100

    Mostly works

--- ham/plugin.replayer.server/src/main/java/org/kendar/replayer/ReplayerStatus.java
+++ ham/plugin.replayer.server/src/main/java/org/kendar/replayer/ReplayerStatus.java
@@ -24,6 +24,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class ReplayerStatus {
