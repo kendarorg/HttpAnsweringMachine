b24f2bf95b03673c4381d003e1c29218a2b70851
Kendar <unknown@kendar.org>
2023-01-12 10:46:49 +0100

    4.1.1

--- scripts/templates/releasebuild/samples/calendar/be.bat
+++ scripts/templates/releasebuild/samples/calendar/be.bat
@@ -16,7 +16,7 @@ pause
 cd %CALENDAR_PATH%\be
 dir /b be*.jar > .temp.txt
 set /p JAR_NAME=<.temp.txt
-start java -cp "be-4.1.0.jar;../janus-driver-1.0.11-SNAPSHOT.jar" org.springframework.boot.loader.JarLauncher --spring.config.location=file:///%cd%\bedbham.application.properties
+start java -cp "be-4.1.1.jar;../janus-driver-1.1.0.jar" org.springframework.boot.loader.JarLauncher --spring.config.location=file:///%cd%\bedbham.application.properties
 REM start java -jar %JAR_NAME%  --spring.config.location=file:///%cd%\bedbham.application.properties
 REM Wait for startup
 timeout /t 10 /nobreak
