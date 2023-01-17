021608ad9df2b5d56d340830f65e7611f8b5569b
Kendar <unknown@kendar.org>
2023-01-17 17:38:10 +0100

    Matchers list

--- ham/app.commons/src/main/java/org/kendar/servers/http/matchers/MatchersRegistry.java
+++ ham/app.commons/src/main/java/org/kendar/servers/http/matchers/MatchersRegistry.java
@@ -1,5 +1,8 @@
 package org.kendar.servers.http.matchers;
 
+import java.util.List;
+
 public interface MatchersRegistry {
     Class<?> get(String key);
+    List<String> getAll();
 }
---
+++ ham/app/src/main/java/org/kendar/servers/utils/MatchersApi.java
@@ -0,0 +1,50 @@
+package org.kendar.servers.utils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.kendar.dns.configurations.DnsConfig;
+import org.kendar.dns.configurations.PatternItem;
+import org.kendar.http.FilteringClass;
+import org.kendar.http.HttpFilterType;
+import org.kendar.http.annotations.HamDoc;
+import org.kendar.http.annotations.HttpMethodFilter;
+import org.kendar.http.annotations.HttpTypeFilter;
+import org.kendar.http.annotations.multi.HamResponse;
+import org.kendar.servers.http.Request;
+import org.kendar.servers.http.Response;
+import org.kendar.servers.http.matchers.MatchersRegistry;
+import org.kendar.utils.ConstantsHeader;
+import org.kendar.utils.ConstantsMime;
+import org.springframework.stereotype.Component;
+
+@Component
+@HttpTypeFilter(hostAddress = "${global.localAddress}", blocking = true)
+public class MatchersApi  implements FilteringClass {
+
+    private MatchersRegistry matchersRegistry;
+
+    public MatchersApi(MatchersRegistry matchersRegistry){
+
+        this.matchersRegistry = matchersRegistry;
+    }
+    ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    public String getId() {
+        return this.getClass().getName();
+    }
+
+    @HttpMethodFilter(phase = HttpFilterType.API,
+            pathAddress = "/api/matchers",
+            method = "GET")
+    @HamDoc(
+            tags = {"base/utils"},
+            description = "Retrieve all matchers",
+            responses = @HamResponse(
+                    body = String[].class
+            ))
+    public void getDnsMappings(Request req, Response res) throws JsonProcessingException {
+        res.addHeader(ConstantsHeader.CONTENT_TYPE, ConstantsMime.JSON);
+        res.setResponseText(mapper.writeValueAsString(matchersRegistry.getAll()));
+    }
+}
