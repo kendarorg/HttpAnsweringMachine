b319a847e5d419f4d1db3968691243d055d81eab
Kendar <unknown@kendar.org>
2023-04-17 16:27:29 +0200

    Download curls

--- ham/plugin.replayer.server/src/main/resources/web/plugins/recording/vcomponents/vlist.vue
+++ ham/plugin.replayer.server/src/main/resources/web/plugins/recording/vcomponents/vlist.vue
@@ -218,28 +218,11 @@ module.exports = {
         allIds.push(row['id']);
       })
       var id = getUrlParameter("id");
-      var toUpload = JSON.stringify(allIds);
       downloadFilePost(
           '/api/plugins/replayer/generator/'+id+'/curl',
-          'test.zip',
+          'recording.'+id+'.curls.zip',
           allIds
       );
-      /*const headers = {'Content-Type': 'application/json',responseType: 'blob'};
-      axiosHandle(axios.post('/api/plugins/replayer/generator/'+id+'/curl', toUpload, {headers}), (response) => {
-        // create file link in browser's memory
-        const href = URL.createObjectURL(new Blob([response.data], { type: "application/zip" }))
-
-        // create "a" HTML element with href to file & click
-        const link = document.createElement('a');
-        link.href = href;
-        link.setAttribute('download', 'curls.zip'); //or any other extension
-        document.body.appendChild(link);
-        link.click();
-
-        // clean up "a" element & remove ObjectURL
-        document.body.removeChild(link);
-        //URL.revokeObjectURL(href);
-      });*/
     }
   }
 }
